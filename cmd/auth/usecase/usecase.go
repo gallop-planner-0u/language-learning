d37a41a5d7742adc3695d24357050358e56e17e7
gallop-planner-0u <gallop-planner-0u@icloud.com>
2023-08-29 22:38:09 +0600

    feat: usecase.go added

---
+++ cmd/auth/usecase/usecase.go
@@ -0,0 +1,50 @@
+package usecase
+
+import (
+	"context"
+	"language-learning/cmd/auth"
+	"language-learning/ent"
+)
+
+type Usecase struct {
+	repo auth.Repository
+}
+
+func New(repo auth.Repository) *Usecase {
+	return &Usecase{
+		repo: repo,
+	}
+}
+
+func (uc *Usecase) SignUp(ctx context.Context, user *ent.User) (int, error) {
+	return uc.repo.Create(ctx, user)
+}
+
+func (uc *Usecase) SignIn(ctx context.Context, username, password string) (string, error) {
+	user, err := uc.repo.Get(ctx, username, password)
+	accessToken, err := uc.GenerateToken(ctx, user)
+	if err != nil {
+		return "", err
+	}
+
+	return accessToken, nil
+}
+
+func (uc *Usecase) ParseToken(accessToken string) (*ent.User, error) {
+	return nil, nil
+}
+
+func (uc *Usecase) GenerateToken(ctx context.Context, user *ent.User) (string, error) {
+	var username, password string
+	_, err := uc.repo.Get(ctx, username, password)
+	if err != nil {
+		return "", err
+	}
+
+	var accessToken string
+	if err != nil {
+		return "", err
+	}
+
+	return accessToken, nil
+}
