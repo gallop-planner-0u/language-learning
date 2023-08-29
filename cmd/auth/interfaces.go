711b9e20aa9241ffe7b7a03d17bf1bfa148d643f
gallop-planner-0u <gallop-planner-0u@icloud.com>
2023-08-29 09:53:11 +0600

    feat: Usecase interface added

--- cmd/auth/interfaces.go
+++ cmd/auth/interfaces.go
@@ -6,3 +6,8 @@ type Repository interface {
 	Get(ctx context.Context, username, passwordHash string) (interface{}, error)
 	Create(ctx context.Context, user interface{}) (int, error)
 }
+
+type Usecase interface {
+	SignIn(ctx context.Context, username, password string) (string, error)
+	SignUp(ctx context.Context, user interface{}) (int, error)
+}
