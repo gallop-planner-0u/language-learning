7fd9773a6c50e2fc84de44d64b5e26fbe32da0e8
gallop-planner-0u <gallop-planner-0u@icloud.com>
2023-09-03 04:28:16 +0600

    feat: main.go

--- cmd/main.go
+++ cmd/main.go
@@ -1,17 +1,49 @@
 package main
 
 import (
-	"net/http"
+	"context"
+	authhandler "language-learning/cmd/auth/controller/http"
+	authrepository "language-learning/cmd/auth/repository"
+	authusecase "language-learning/cmd/auth/usecase"
+	"language-learning/pkg"
+	"log"
+	"os"
 
 	"github.com/gin-gonic/gin"
+	_ "github.com/lib/pq"
+	"github.com/spf13/viper"
 )
 
 func main() {
+	InitConfig()
+	client := pkg.GetEntClient()
+	defer client.Close()
+
+	if err := client.Schema.Create(context.Background()); err != nil {
+		log.Fatal(err)
+	}
+
+	_authRepo := authrepository.New(client)
+	_authUsecase := authusecase.New(_authRepo)
+	_authHandler := authhandler.New(_authUsecase)
+
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
-		})
-	})
-	r.Run(":8080")
+	authhandler.RegisterRoutes(r, _authHandler)
+
+	r.Run(":" + viper.GetString("app.port"))
+}
+
+func InitConfig() {
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+	configPath := os.Getenv("CONFIG_PATH")
+	if configPath == "" {
+		configPath = "../config"
+	}
+	viper.AddConfigPath(configPath)
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(err)
+	}
 }
