a535f02447265f8e4f2cb078abd7b535586834d8
gallop-planner-0u <gallop-planner-0u@icloud.com>
2023-09-03 14:30:31 +0600

    feat: user's type changed, ParseToken() added

--- cmd/auth/interfaces.go
+++ cmd/auth/interfaces.go
@@ -3,14 +3,16 @@ package auth
 import (
 	"context"
 	"language-learning/ent"
+	"language-learning/models"
 )
 
 type Repository interface {
 	Get(ctx context.Context, username, passwordHash string) (*ent.User, error)
-	Create(ctx context.Context, user interface{}) (int, error)
+	Create(ctx context.Context, user *ent.User) (int, error)
 }
 
 type Usecase interface {
 	SignIn(ctx context.Context, username, password string) (string, error)
-	SignUp(ctx context.Context, user interface{}) (int, error)
+	SignUp(ctx context.Context, user *ent.User) (int, error)
+	ParseToken(accessToken string) (*models.CustomClaims, error)
 }
