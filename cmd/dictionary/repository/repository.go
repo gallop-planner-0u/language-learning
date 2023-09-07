27af2fb32e36352fcdb9c0094e2fa8bac4b0f8ac
gallop-planner-0u <gallop-planner-0u@icloud.com>
2023-09-08 04:02:10 +0600

    fix: repository.go

--- cmd/dictionary/repository/repository.go
+++ cmd/dictionary/repository/repository.go
@@ -15,8 +15,22 @@ func New(client *ent.Client) *Repository {
 	}
 }
 
-func (r *Repository) GetByID(ctx context.Context, userID, id int) (*ent.Record, error)
-func (r *Repository) GetByWord(ctx context.Context, userID int, word string) (*ent.Record, error)
-func (r *Repository) Create(ctx context.Context, userID int, record *ent.Record) (int, error)
-func (r *Repository) Update(ctx context.Context, userID, id int, updatedRecord *ent.Record) error
-func (r *Repository) Delete(ctx context.Context, userID, id int) error
+func (r *Repository) GetByID(ctx context.Context, userID, id int) (*ent.Record, error) {
+	return nil, nil
+}
+
+func (r *Repository) GetByWord(ctx context.Context, userID int, word string) (*ent.Record, error) {
+	return nil, nil
+}
+
+func (r *Repository) Create(ctx context.Context, userID int, record *ent.Record) (int, error) {
+	return 0, nil
+}
+
+func (r *Repository) Update(ctx context.Context, userID, id int, updatedRecord *ent.Record) error {
+	return nil
+}
+
+func (r *Repository) Delete(ctx context.Context, userID, id int) error {
+	return nil
+}
