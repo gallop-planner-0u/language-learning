39a6d8f96276319465e5317fe3f415f03d4d94c2
gallop-planner-0u <gallop-planner-0u@icloud.com>
2023-09-08 01:12:52 +0600

    feat: Usecase methods added

--- cmd/dictionary/usecase/usecase.go
+++ cmd/dictionary/usecase/usecase.go
@@ -16,3 +16,22 @@ func New(repo dictionary.Repository) *Usecase {
 	}
 }
 
+func (uc *Usecase) GetByID(ctx context.Context, userID, id int) (*ent.Record, error) {
+	return uc.repo.GetByID(ctx, userID, id)
+}
+
+func (uc *Usecase) GetByWord(ctx context.Context, userID int, word string) (*ent.Record, error) {
+	return uc.repo.GetByWord(ctx, userID, word)
+}
+
+func (uc *Usecase) Create(ctx context.Context, userID int, record *ent.Record) (int, error) {
+	return uc.repo.Create(ctx, userID, record)
+}
+
+func (uc *Usecase) Update(ctx context.Context, userID, id int, updatedRecord *ent.Record) error {
+	return uc.repo.Update(ctx, userID, id, updatedRecord)
+}
+
+func (uc *Usecase) Delete(ctx context.Context, userID, id int) error {
+	return uc.repo.Delete(ctx, userID, id)
+}
