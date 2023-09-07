bc5e9387c4af915ae1e45fc4d6b94d393446a6c8
gallop-planner-0u <gallop-planner-0u@icloud.com>
2023-09-08 01:11:25 +0600

    feat: Repository method added

--- cmd/dictionary/repository/repository.go
+++ cmd/dictionary/repository/repository.go
@@ -15,3 +15,8 @@ func New(client *ent.Client) *Repository {
 	}
 }
 
+func (r *Repository) GetByID(ctx context.Context, userID, id int) (*ent.Record, error)
+func (r *Repository) GetByWord(ctx context.Context, userID int, word string) (*ent.Record, error)
+func (r *Repository) Create(ctx context.Context, userID int, record *ent.Record) (int, error)
+func (r *Repository) Update(ctx context.Context, userID, id int, updatedRecord *ent.Record) error
+func (r *Repository) Delete(ctx context.Context, userID, id int) error
