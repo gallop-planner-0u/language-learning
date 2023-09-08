7de006c6d2001078ff64a577b97574971e314257
gallop-planner-0u <gallop-planner-0u@icloud.com>
2023-09-08 19:20:51 +0600

    feat: Create() method implemented

--- cmd/dictionary/repository/repository.go
+++ cmd/dictionary/repository/repository.go
@@ -23,8 +23,14 @@ func (r *Repository) GetByWord(ctx context.Context, userID int, word string) (*e
 	return nil, nil
 }
 
-func (r *Repository) Create(ctx context.Context, userID int, record *ent.Record) (int, error) {
-	return 0, nil
+func (r *Repository) Create(ctx context.Context, userID int, newRecord *ent.Record) (int, error) {
+	_r, err := r.client.Record.
+		Create().
+		SetUserID(userID).
+		SetWord(newRecord.Word).
+		SetTranslation(newRecord.Translation).
+		Save(ctx)
+	return _r.ID, err
 }
 
 func (r *Repository) Update(ctx context.Context, userID, id int, updatedRecord *ent.Record) error {
